037e8af23ad9b9110a0b2f66de75fcd8721bad7d
supapraisekb <praisenimize@gmail.com>
2023-02-18 20:53:10 +0000

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int base_10;
+
+	for (base_10 = 0; base_10 < 10; base_10++)
+	{
+		printf("%d", base_10);
+	}
+	return (0);
+}
