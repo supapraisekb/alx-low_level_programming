1fe6f153dcb04a7b02f9477d5d53e9a15bc6822d
supapraisekb <praisenimize@gmail.com>
2023-02-21 22:31:57 +0000

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints out the alphabet
+ * main - uses built in function
+ * Return: 0 if succesful
+ */
+
+void print_alphabet(void)
+{
+char alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+_putchar(alph);
+_putchar('\n');
+}
