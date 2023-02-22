9961ead5c93f0565b8dcbf43e63dda9fcff1f515
supapraisekb <praisenimize@gmail.com>
2023-02-22 09:15:37 +0000

    7-last_digit.c

--- 0x02-functions_nested_loops/7-last_digit.c
+++ 0x02-functions_nested_loops/7-last_digit.c
@@ -2,17 +2,16 @@
 
 /**
 *print_last_digit - extract last number of a variable
-* @last_d: variable where we extract last number
+* @x: variable where we extract last number
 * Return: last_d if succesful
 */
 
-int print_last_digit(int last_d)
+int print_last_digit(int x)
 {
-	last_d %= 10;
-	if (last_d < 0)
-		last_d *= -1;
-	last_d += 48;
-	_putchar(last_d);
-	return (last_d - '0');
+	int last_d = x % 10;
+	if (x < 0)
+		x *= -1;
+	_putchar('0' + last_d);
+	return (last_d);
 }
 
