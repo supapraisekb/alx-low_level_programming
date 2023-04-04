37fd279eaa4469732998ecc56de622512723c442
Ihendu Praise Eze <praisenimize@gmail.com>
2023-04-04 00:47:13 +0000

    task 5 completed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 void print_before(void) __attribute__ ((constructor));
 
 /**
-* before_main - Function that prints a message before main is executed
+* print_before - Function that prints a message before main is executed
 *
 * This function is executed before the main function and prints the message
 */
