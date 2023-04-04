92f4befeb181ef8583854860df506fa80e12878e
Ihendu Praise Eze <praisenimize@gmail.com>
2023-04-04 01:24:49 +0000

    hello-holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,21 @@
-section .rodata
-msg db 'Hello, Holberton',10,0
-fmt db '%s',10,0
-
-
-section .text
-global main
 extern printf
 
+section .text
+   global main
+
 main:
-push msg
-push fmt
-call printf
-add rsp,16
-xor eax,eax
-ret
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
