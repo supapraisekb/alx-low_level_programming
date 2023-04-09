43268cf15b256d38668174026345655c1ad5aa43
Ihendu Praise Eze <praisenimize@gmail.com>
2023-04-09 17:21:04 +0000

    cleaned code for task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,21 +7,13 @@
   *Return: the value of a given bit at index
   * If an error occurs, it returns -1
   */
-/**int get_bit(unsigned long int n, unsigned int index)
-{
-
-	int position = sizeof(unsigned long int) * 8 - 1;
-
-	if (index >> position)
-
-		return (-1);
-	return ((n >> index) & 1);
-}
-**/
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-if (index > 63)
+int position = sizeof(unsigned long int) * 8 - 1;
+
+if (index >> position)
 return (-1);
 return ((n >> index) & 1);
 }
+
