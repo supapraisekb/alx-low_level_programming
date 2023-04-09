65413334f976fe2c6a0f3910ddeadcd46957a758
Ihendu Praise Eze <praisenimize@gmail.com>
2023-04-09 13:19:30 +0000

    cleaned main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,7 @@
 /*
 *Filename: Main.h
 *Description: Contains all prototypes for the fucntions
-*    	      required for task 0x14-bit manipulation
+*		required for task 0x14-bit manipulation
 */
 
 int _putchar(char c);
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsined long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n,
 				unsigned long int m);
 int get_endianness(void);
