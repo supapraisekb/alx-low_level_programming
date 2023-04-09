a212b87f5edbf92f6a66be41bae528dab2f09e8d
Ihendu Praise Eze <praisenimize@gmail.com>
2023-04-09 21:35:20 +0000

    fixed betty error

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
-  *set_bit - It sets the value of a bit to 0 at a given index
-  *@n: Points to a bit within the function
-  *@index: the index of the bit to set beginning at 0
-  *
-  *Return: returns 1 if successful, -1 on error
-  */
+*set_bit - It sets the value of a bit to 0 at a given index
+*@n: Points to a bit within the function
+*@index: the index of the bit to set beginning at 0
+*
+*Return: returns 1 if successful, -1 on error
+*/
 int set_bit(unsigned long int *n, unsigned int index)
 {
 unsigned int position = sizeof(n) * 8;
 
 if (index >= position || !n)
-		return (-1);
+return (-1);
 
-*n |= (1ul << index);
+*n = *n | (1 << index);
 return (1);
 }
