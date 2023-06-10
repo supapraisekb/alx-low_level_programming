88408be59d90716b1319f397b6e1206228009d55
Ihendu Praise Eze <praisenimize@gmail.com>
2023-06-10 06:10:24 +0000

    fixed betty

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,20 +2,18 @@
 
 /**
  * print_last_digit - extract last number of a variable
- * @x: variable where we extract last number
- * Return: l_d if succesful
+ * @num: variable where we extract last number
+ * Return: last_digit if succesful
  */
 
 int print_last_digit(int num)
-
 {
 int last_digit;
 if (num < 0)
 {
 num *= -1;
 }
-
 last_digit = num % 10;
-_putchar(last_digit + '0');
-return(last_digit);
+_putchar (last_digit + '0');
+return (last_digit);
 }
