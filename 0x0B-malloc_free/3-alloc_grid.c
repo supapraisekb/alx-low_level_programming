4bab1faadc396a8df9e4c54fd38666d894ba9d0b
Ihendu Praise Eze <praisenimize@gmail.com>
2023-06-13 23:48:48 +0000

    grg function

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,28 +13,28 @@ int **alloc_grid(int width, int height)
 
 {
 int **grid;
-int x, y;
+int u, v;
 
 if (width <= 0 || height <= 0)
 return (NULL);
 grid = malloc(sizeof(int *) * height);
 if (grid == NULL)
 return (NULL);
-for (x = 0; x < height; x++)
+for (u = 0; u < height; u++)
 {
-grid[x] = malloc(sizeof(int) * width);
-if (grid[x] == NULL)
+grid[u] = malloc(sizeof(int) * width);
+if (grid[u] == NULL)
 {
-for (; x >= 0; x--)
-free(grid[x]);
+for (; u >= 0; u--)
+free(grid[u]);
 free(grid);
 return (NULL);
 }
 }
-for (x = 0; x < height; x++)
+for (u = 0; u < height; u++)
 {
-for (y = 0; y < width; y++)
-grid[x][y] = 0;
+for ( = 0; v < width; v++)
+grid[u][v] = 0;
 }
 return (grid);
 }
