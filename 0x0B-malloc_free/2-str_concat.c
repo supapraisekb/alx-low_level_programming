9c423ad56867a9bd58afc01ab051c2de1b7b2ead
Ihendu Praise Eze <praisenimize@gmail.com>
2023-06-13 23:30:57 +0000

    string concatenation function

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,32 +10,32 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr;
-	unsigned int x, len1 = 0, len2 = 0;
+	char *p;
+	unsigned int z, len_1 = 0, len_2 = 0;
 
 	if (!s1)
 		s1 = "";
 	else
-		for (; s1[len1]; len1++)
+		for (; s1[len_1]; len_1++)
 			;
 	if (!s2)
 		s2 = "";
 	else
-		for (; s2[len2]; len2++)
+		for (; s2[len_2]; len_2++)
 			;
-	len2++;
-	ptr = malloc((len1 + len2) * sizeof(char));
+	len_2++;
+	ptr = malloc((len_1 + len_2) * sizeof(char));
 
-	if (!ptr)
+	if (!p)
 		return (NULL);
 
-	for (x = 0; x <= (len1 + len2); x++)
+	for (z = 0; z <= (len_1 + len_2); z++)
 	{
-		if (x < len1)
-			ptr[x] = s1[x];
+		if (z < len_1)
+			p[z] = s1[z];
 		else
-			ptr[x] = s2[x - len1];
+			ptr[x] = s2[z - len_1];
 	}
 
-	return (ptr);
+	return (p);
 }
