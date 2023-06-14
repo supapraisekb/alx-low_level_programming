697e68fbff38165765a65958e867eebabc974f06
Ihendu Praise Eze <praisenimize@gmail.com>
2023-06-14 00:15:08 +0000

    fre drid fucntion

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid/array
- * @grid: two dimensional grid/array
- * @height: rows
- */
+* free_grid - frees a 2 dimensional grid/array
+* @grid: two dimensional grid/array
+* @height: rows
+*/
 
 void free_grid(int **grid, int height)
 {
-	int i;
+int i;
 
-	 if (grid == NULL || height <= 0)
-	 return;
+if (grid == NULL || height <= 0)
+return;
 
-	for (i = 0; i < height; i++)
-		free(grid[i]);
+for (i = 0; i < height; i++)
+free(grid[i]);
 
-	free(grid);
+free(grid);
 }
