87cb61d3594ca3872a988ea669b774eadfcbc0a1
Ihendu Praise Eze <praisenimize@gmail.com>
2023-06-16 00:52:57 +0000

    code fixes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	p = malloc(nmemb * size);
 
-	if (p == NULL)
+	if (!p)
 		return (NULL);
 
 	for (count = 0; count < nmemb * size; count++ )
