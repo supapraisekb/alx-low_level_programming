6f6550b776220aaba2dc8a43d080e035473b8513
Ihendu Praise Eze <praisenimize@gmail.com>
2023-06-16 02:25:54 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,50 +4,51 @@
 #include <string.h>
 
 /**
- * _realloc - reallocates a memory block using malloc and free (realloc)
- * @ptr: pointer
- * @old_size: size of old pointer
- * @new_size: size of new pointer
- * Return: new void pointer or NULL if it fails
- */
-
-
-
+* _realloc - reallocates a memory block using malloc and free (realloc)
+* @ptr: pointer
+* @old_size: size of old pointer
+* @new_size: size of new pointer
+* Return: new void pointer or NULL if it fails
+*/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *p;
-	unsigned int z;
+void *p;
+unsigned int z;
 
-	if (!ptr)
-	{
-		p = malloc(new_size);
-		if (!p)
-			return (NULL);
-		return (p);
-	}
+if (!ptr)
+{
+p = malloc(new_size);
+if (!p)
+return (NULL);
+return (p);
+}
 
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
 
-	if (new_size == old_size)
-		return (ptr);
+if (new_size == old_size)
+return (ptr);
 
-	p = malloc(new_size);
+p = malloc(new_size);
 
-	if (!p)
-		return (NULL);
+if (!p)
+return (NULL);
 
-	if (new_size < old_size)
-		for (; z < new_size; z++)
-			((char *)p)[z] = ((char *)ptr)[z];
-	else
-		for (; z < old_size; z++)
-			((char *)p)[z] = ((char *)ptr)[z];
+if (new_size < old_size)
+{
+for (z = 0; z < new_size; z++)
+((char *)p)[z] = ((char *)ptr)[z];
+}
+else
+{
+for (z = 0; z < old_size; z++)
+((char *)p)[z] = ((char *)ptr)[z];
+}
 
-	free(ptr);
+free(ptr);
 
-	return (p);
+return (p);
 }
