fa10cfe60261ecf9ae8b58c779be6464f570fba3
Ihendu Praise Eze <praisenimize@gmail.com>
2023-06-22 02:19:50 +0000

    1-array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array && action && size)
+	if (array !=NULL  && action && size)
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }
