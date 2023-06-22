7d5450de4e5b88a124538f3d6ad67d02e9e22587
Ihendu Praise Eze <praisenimize@gmail.com>
2023-06-22 01:56:57 +0000

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,11 +3,11 @@
 /**
  * print_name - this function prints a name
  * @name: taken as an argument of to be printed
- * @f: it is the pointer to a function
+ * @fun_ptr: it is the pointer to a function
  */
 
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*fun_ptr)(char *))
 {
-	if (f && name)
-		f(name);
+	if (fun_ptr && name)
+		fun_ptr(name);
 }
