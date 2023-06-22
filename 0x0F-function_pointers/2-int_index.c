db1720c5b16146e115b5bf15afc46ba2b9a05dac
Ihendu Praise Eze <praisenimize@gmail.com>
2023-06-22 16:57:02 +0000

    2-int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,19 +2,19 @@
 
 /**
  * int_index - function that searches for an integer
- * @array: array of integers
- * @size: size of array
- * @cmp: pointer to the function to be used to compare values
- * Return: return index where match is found or -1 if fails
+ * @int_array: array of integers
+ * @arr_size: size of array
+ * @cmp_ptr: pointer to the function to be used to compare values
+ * Return: index where match is found or -1 if fails
  */
 
-int int_index(int *array, int size, int (*cmp)(int))
+int int_index(int *int_array, int arr_size, int (*cmp_ptr)(int))
 {
 	int i;
 
-	if (array && cmp && size > 0)
+	if (int_array != NULL && cmp_ptr != NULL && arr_size > 0)
 		for (i = 0; i < size; i++)
-			if (cmp(array[i]))
+			if (cmp_ptr(int_array[i]))
 				return (i);
 	return (-1);
 }
