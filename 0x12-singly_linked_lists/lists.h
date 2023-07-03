1626281cfa76bddaa5e437e6995a5455640410fd
Ihendu Praise Eze <praisenimize@gmail.com>
2023-07-03 23:00:28 +0000

    lists edited

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,20 +4,20 @@
 #include <stddef.h>
 
 /**
- * struct list_s - singly linked list
- * @string: string
- * @lenof_str: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for ALX project
- */
+* struct list_s - singly linked list
+* @str: string
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+* for ALX project
+*/
 typedef struct list_s
 {
 char *str;
 unsigned int len;
- struct list_s *next;
-  }list_t;
+struct list_s *next;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
@@ -25,6 +25,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-int _strlen(const char *str)
 
 #endif /*LISTS_H*/
