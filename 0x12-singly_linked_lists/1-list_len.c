f5ec8c0141ee4258326a5ced609489ccb256d4a5
Ihendu Praise Eze <praisenimize@gmail.com>
2023-07-03 20:48:45 +0000

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,13 +9,12 @@
 */
 size_t list_len(const list_t *h)
 {
+size_t element_count = 0;
 
-size_t  j = 0;
-
-while (h != NULL)
+for (; h != NULL; h = h->next)
 {
-j++;
-h = h->next;
+element_count++;
 }
-return (j);
+
+return (element_count);
 }
