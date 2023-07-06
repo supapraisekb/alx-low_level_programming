737600df8090f33599143f4f1a79ac76b3e2d87e
Ihendu Praise Eze <praisenimize@gmail.com>
2023-07-06 02:34:53 +0000

    included empty list checker

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * add_nodeint - Adds a new node at the beginning of a listint_t list.
+ * add_nodeint - Adds a new node at the beginning of a list.
  *
- * @head: A pointer to a pointer to the head of the list. it is the first node
- * @n: The integer value to store in the new node.
+ * @head: the first node
+ * @n: The integer to be stored in the new node.
  *
  * Return: If successful, return the address of the new element.
  * On failure, return NULL.
@@ -14,7 +14,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 listint_t *newNode;
 
 if (head == NULL)
+{
+printf("empty list");
 return (NULL);
+}
 newNode = malloc(sizeof(listint_t));
 if (!newNode)
 return (NULL);
