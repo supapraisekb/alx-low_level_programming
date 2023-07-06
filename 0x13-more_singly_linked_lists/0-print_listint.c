38984a02584e496e73d47e2004fb6ddde855688a
Ihendu Praise Eze <praisenimize@gmail.com>
2023-07-06 01:33:16 +0000

    added the NULL checker

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,15 @@
 */
 size_t print_listint(const listint_t *h)
 {
-size_t node_count = 0;
+size_t each_node_count = 0;
 
+if (h == NULL)
+printf("the list is empty\n");
 while (h != NULL)
 {
 printf("%d\n", h->n);
 h = h->next;
-node_count++;
+each_node_count++;
 }
-return (node_count);
+return (each_node_count);
 }
