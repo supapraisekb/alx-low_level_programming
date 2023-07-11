60c13576661fd903463723d46eedf5eb0ebd8b98
Ihendu Praise Eze <praisenimize@gmail.com>
2023-07-11 19:47:32 +0000

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,53 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
-* read_textfile - reads a text file and prints it to the POSIX stdout
-* @filename: file to read
-* @letters: number of letters to be read and printed
-*
-* Return: number of letters that can be read and printed
-* On fail or if filename is NULL returns 0
-*/
+  * read_textfile - reads a text file and prints it to the POSIX stdout
+  * @filename: file to read
+  * @letters: number of letters to be read and printed
+  *
+  * Return: number of letters that can be read and printed
+  * On fail or if filename is NULL returns 0
+  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int file_D, rBytes, wBytes;
-char *buff_size;
-
-/* Check for errors in filename */
-if (!filename)
+if (filename == NULL)
+return (0);
+int fd = open(filename, O_RDONLY);
+if (fd == -1)
 return (0);
 
-/* file opening */
-file_D = open(filename, O_RDONLY);
-if (file_D == -1)
+char *buffer = (char *) malloc((letters + 1) * sizeof(char));
+if (buffer == NULL)
+close(fd);
 return (0);
-/* Allocate Buffer Size */
-buff_size = malloc(sizeof(char) * letters);
-if (!buff_size)
+
+
+ssize_t bytes_read = read(fd, buffer, letters);
+if (bytes_read == -1)
 {
-close(file_D);
+close(fd);
+free(buffer);
 return (0);
 }
-/* Read the bytes of Text from the file */
-rBytes = read(file_D, buff_size, letters);
-if (rBytes == -1)
+
+ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
 {
-free(buff_size);
-close(file_D);
+close(fd);
+free(buffer);
 return (0);
 }
-/* Write the bytes of text to stdout */
-wBytes = write(STDOUT_FILENO, buff_size, rBytes);
-if (wBytes == -1 || wBytes != rBytes)
-{
-free(buff_size);
-close(file_D);
-return (0);
+close(fd);
+free(buffer);
+return (bytes_written);
 }
-free(buff_size);
-close(file_D);
-return (rBytes);
-}
+
